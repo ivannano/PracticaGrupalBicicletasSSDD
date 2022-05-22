ffea7455380b8e1fa2f9c1e93ea806f202a2212e
ivannano <iacebedoherrera@gmail.com>
2022-05-22 18:48:58 +0200

    v0.1 Terminado

--- PracticaGrupalBicicletasSSDD/src/main/java/es/codeurjc/PracticaGrupalBicicletasSSDD/Estaciones/Station.java
+++ PracticaGrupalBicicletasSSDD/src/main/java/es/codeurjc/PracticaGrupalBicicletasSSDD/Estaciones/Station.java
@@ -11,6 +11,8 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import es.codeurjc.PracticaGrupalBicicletasSSDD.Bicicletas.Bicycle;
 
 
@@ -27,6 +29,7 @@ public class Station {
 		private EstadoStation estado;
 		private int capacidad;
 		@OneToMany(mappedBy="estacionAsig")
+		@JsonIgnore
 		private List<Bicycle> bicicletas;
 		
 		public enum EstadoStation{
--- PracticaGrupalBicicletasSSDD/src/main/java/es/codeurjc/PracticaGrupalBicicletasSSDD/Reserva/RestControllerReserva.java
+++ PracticaGrupalBicicletasSSDD/src/main/java/es/codeurjc/PracticaGrupalBicicletasSSDD/Reserva/RestControllerReserva.java
@@ -75,6 +75,7 @@ public class RestControllerReserva {
 	}
 	
 //*************************************************************************************************
+	
 	@Operation(summary = "Create a new booking")
 	@ApiResponses(value = {
 			@ApiResponse(responseCode = "200", description = "Booking created succesfully", content = {
@@ -82,27 +83,14 @@ public class RestControllerReserva {
 			@ApiResponse(responseCode = "404", description = "Station and bicycle don´t exist or don´t meet the conditions", content = @Content) })
 	
 	@PostMapping("/reserva")
-	public ResponseEntity<Reserva> newReserve(@RequestBody Reserva newReserve){
+	
+	public ResponseEntity<Reserva> newReserve(@RequestBody Reserva newReserve) {
 		
 		long id_estacion = newReserve.getId_estacion();			//Id de la estacion
 		long id_bicicleta = newReserve.getId_bicicleta();		//Id de la bicicleta
 		long id_usuario = newReserve.getId_usuario();	 		//Id del usuario
 		
-		String url = URL_API_USUARIOS + id_usuario; 			//http://localhost:8081/users/id_usuario
-		
 		RestTemplate restTemplate = new RestTemplate();			//Objeto RestTemplate para conectarnos a la API
-		
-		/*HttpHeaders headers = new HttpHeaders();					
-	    headers.setContentType(MediaType.APPLICATION_JSON);				 
-	    HttpEntity<Object> entity = new HttpEntity<Object>(headers);
-	    
-	    ResponseEntity<String> out = restTemplate.exchange(url, HttpMethod.GET, entity, String.class); //Respuesta de la llamada a la API
-	    */
-	    /*if(!out.getStatusCode().equals(ResponseEntity.ok())) {  	//Comprobamos que el codigo de estado es el esperado
-	    	return ResponseEntity.notFound().build();				//Mandamos la respuesta de NO ENCONTRADO
-	    }else {*/
-			//User user = restTemplate.getForObject(url,User.class);
-			ObjectNode user = restTemplate.getForObject(url,ObjectNode.class); 	//Obtenemos el objecto de la peticion
 	
 			Optional<Station> s = stationService.findById(id_estacion);			//Obtenemos la estacion con el ID
 			Optional<Bicycle> b = bicycleService.findById(id_bicicleta);		//Obtenemos la bicicleta con el ID
@@ -111,82 +99,86 @@ public class RestControllerReserva {
 				if (s.get().getEstado().equals(EstadoStation.ACTIVO) && 		//Comprobamos los estados de la estacion y la estacion a la que se asigna la bicicleta		
 							b.get().getEstado().equals(Estado.EN_BASE) && b.get().getEstacionAsig().getId() == id_estacion) {
 					
-					reservaService.save(newReserve);							//Guardamos la reserva en la base de datos
 					URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")	
 							.buildAndExpand(newReserve.getId_reserva()).toUri();
-					//user.setSaldo(user.getSaldo()-10);
-					//restTemplate.put(url, user);	
-					double nuevoSaldo = user.get("saldo").asDouble()-10;		//Le restamos lo que cuesta la reserva de la bicicleta
-					user.put("saldo", nuevoSaldo);								//Modificamos el campo "saldo" con el nuevo saldo
-					restTemplate.put(url, user);								//Modificamos el objeto de la API extenrna con el nuevo objeto modificado
-					b.get().setEstado(Estado.RESERVADA);
-					bicycleService.save(b.get());								//Ponemos la bicicleta como reservada
 					
-					s.get().remove(b.get());									//Quitamos la bicicleta de su estacion
-					stationService.save(s.get());
+					String url = "http://localhost:8081/users/bookings/" + id_usuario;
+					ResponseEntity<ObjectNode> respuesta = restTemplate.exchange(url, HttpMethod.PUT, new HttpEntity<>(new HttpHeaders()), ObjectNode.class);
+					if(respuesta.getStatusCode().equals(HttpStatus.OK)) {
+						reservaService.save(newReserve);							//Guardamos la reserva en la base de datos
+						b.get().setEstado(Estado.RESERVADA);
+						bicycleService.save(b.get());								//Ponemos la bicicleta como reservada
+						s.get().remove(b.get());									//Quitamos la bicicleta de su estacion
+						stationService.save(s.get());
+						return ResponseEntity.created(location).body(newReserve);
+					}
+					else {
+						return ResponseEntity.badRequest().build();
+					}
 					
-					return ResponseEntity.created(location).body(newReserve);
-				}else
+				}
+				else {
 					return ResponseEntity.notFound().build();
+				}
 				
 			}else
 				return ResponseEntity.notFound().build();
 	
-		//}
 	}
 	
 //*************************************************************************************************
+	
 	@Operation(summary = "Delete an existing booking by its id")
 	@ApiResponses(value = {
 			@ApiResponse(responseCode = "200", description = "Found the user", content = {
 					@Content(mediaType = "application/json") }),
 			@ApiResponse(responseCode = "404", description = "Booking not found, or station and bicycle don´t meet the conditions", content = @Content) })
 	@DeleteMapping("/reserva/{id_reserva}")
-	public ResponseEntity<Reserva> deleteReserve(@PathVariable Long id_reserva){
-		
-		Optional<Reserva> reserva = reservaService.findById(id_reserva); 
-		if (reserva.isPresent()) {
-			long id_estacion = reserva.get().getId_estacion();		//Id de la estacion
+	
+	public ResponseEntity<Reserva> deleteReserve(@PathVariable Long id_reserva){		
+		Optional<Reserva> reserva = reservaService.findById(id_reserva);
+		if(reserva.isPresent()) {
+			long id_estacion = reserva.get().getId_estacion();			//Id de la estacion
 			long id_bicicleta = reserva.get().getId_bicicleta();		//Id de la bicicleta
 			long id_usuario = reserva.get().getId_usuario();	 		//Id del usuario
-			String url = URL_API_USUARIOS + id_usuario; 			//http://localhost:8081/users/id_usuario
+			
+			String url = "http://localhost:8081/users/devolucion_bookings/" + id_usuario;
 			
 			RestTemplate restTemplate = new RestTemplate();			//Objeto RestTemplate para conectarnos a la API
 			
-			ObjectNode user = restTemplate.getForObject(url,ObjectNode.class); 	//Obtenemos el objecto de la peticion
-			
 			Optional<Station> s = stationService.findById(id_estacion);			//Obtenemos la estacion con el ID
 			Optional<Bicycle> b = bicycleService.findById(id_bicicleta);		//Obtenemos la bicicleta con el ID
-				
-			if (s.isPresent() && b.isPresent()) { 
-				if (s.get().getEstado().equals(EstadoStation.ACTIVO) && 																
-						b.get().getEstado().equals(Estado.RESERVADA) && b.get().getEstacionAsig().getId() == id_estacion 
-									&& s.get().getBicicletas().size()<s.get().getCapacidad()) { 
-
-					reservaService.deleteById(reserva.get().getId_reserva()); 
-
-					double nuevoSaldo = user.get("saldo").asDouble() + 2; 
-																			
-					user.put("saldo", nuevoSaldo); 
-					restTemplate.put(url, user); 
-					b.get().setEstado(Estado.EN_BASE);
-					bicycleService.save(b.get()); 
-
-					s.get().addBicycle(b.get());
-					stationService.save(s.get());
-
-					//return ResponseEntity.created(location).body(newReserve);
-					return ResponseEntity.ok().build();
+			
+			if (s.isPresent() && b.isPresent()) { 								//Comprobamos que ambos objetos estan presentes
+				if (s.get().getEstado().equals(EstadoStation.ACTIVO) &&
+                        b.get().getEstado().equals(Estado.RESERVADA) && b.get().getEstacionAsig().getId() == id_estacion 
+                                    && s.get().getBicicletas().size()<s.get().getCapacidad()) {
 					
-				} else
+					ResponseEntity<ObjectNode> respuesta = restTemplate.exchange(url, HttpMethod.PUT, new HttpEntity<>(new HttpHeaders()), ObjectNode.class);
+					if(respuesta.getStatusCode().equals(HttpStatus.OK)) {
+						reservaService.deleteById(reserva.get().getId_reserva());	//Guardamos la reserva en la base de datos
+						b.get().setEstado(Estado.EN_BASE);
+						bicycleService.save(b.get());								//Ponemos la bicicleta como reservada
+						s.get().addBicycle(b.get());								//Quitamos la bicicleta de su estacion
+						stationService.save(s.get());
+						return ResponseEntity.ok().build();
+					}
+					else {
+						return ResponseEntity.badRequest().build();
+					}
+					
+				}
+				else {
 					return ResponseEntity.notFound().build();
-
-			} else
+				}
+				
+			}else
 				return ResponseEntity.notFound().build();
-		}else {
+		}
+		else {
 			return ResponseEntity.notFound().build();
 		}
-				
+						
 	}
 		
 }
